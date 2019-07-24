5cc683860b3685379a1deefbdfa50b37c90bdcbe
Serhii <davydko.serhiy@gmail.com>
2019-07-24 11:20:21 +0300

    Updates

---
+++ precache-manifest.e50ffcc179715efc8d55df6ab9ef9600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28325ad6493c0758b636ad0e3565123",
+    "url": "/React_guardian/index.html"
+  },
+  {
+    "revision": "79550d0cc2a88b252b49",
+    "url": "/React_guardian/static/css/main.54bcb2e1.chunk.css"
+  },
+  {
+    "revision": "0f1c5798ca0a25facf8c",
+    "url": "/React_guardian/static/js/2.d5ed7b26.chunk.js"
+  },
+  {
+    "revision": "79550d0cc2a88b252b49",
+    "url": "/React_guardian/static/js/main.0ed405fd.chunk.js"
+  },
+  {
+    "revision": "dbe7171d86849facb3d0",
+    "url": "/React_guardian/static/js/runtime~main.42c35a68.js"
+  }
+]);
